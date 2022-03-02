56ab28c9fefbbad35d87c0909fd4cc18aaed7f5a
JeffGolota07 <jgolo326@mail.endicott.edu>
2022-03-02 12:49:42 -0500

    "Metadata"

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Jeff Golota - Developer & Designer`,
-    siteUrl: `https://www.jeffgolota.com/`
+    url: 'https://www.jeffgolota.com',
+    title: 'Jeff Golota - Developer & Designer',
+    description: 'Hi Im Jeff Golota, come check out what I can do',
   },
   plugins: ["gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
     resolve: 'gatsby-source-filesystem',
