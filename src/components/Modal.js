dac77af6c5dc7b2bef86279d079f222b1a9c79d3
JeffGolota07 <jgolo326@mail.endicott.edu>
2022-03-29 20:59:30 -0400

    "Fixed images on mobile devices"

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -21,23 +21,13 @@ class Modal extends React.Component {
             <h1 className="close" onClick={() => {this.props.onCloseModal()}} style={{color: "#fff", cursor: "pointer"}}>X</h1>
             <div className="image-gallery">
               <h1 className="back" onClick={() => {
-                console.log("Current Index On Click: " + this.state.currentIndex);
                 if (this.state.currentIndex > 0) {
-                  console.log("ClassList Before Change: " + document.querySelector(".image").classList);
-                  console.log("Current Image before removal: " + this.state.currentImage);
                   document.querySelector(".image").classList.remove(this.state.currentImage);
-                  console.log("ClassList After Removal: " + document.querySelector(".image").classList);
                   this.setState({currentIndex: this.state.currentIndex - 1}, () => {
                     this.setState({currentImage: this.props.projectData.images[this.state.currentIndex]}, () => {
                       document.querySelector(".image").classList.add(this.state.currentImage);
                     });
                   });
-                  
-                  console.log("Current Index After Change :" + this.state.currentIndex);
-                  console.log("Current Image After Change :" + this.state.currentImage);
-                  
-                  console.log("ClassList After Adding New Image: " + document.querySelector(".image").classList);
-                  
                 }
               }}>&lt;</h1> 
               <img className="image goingup0"></img>
@@ -50,7 +40,6 @@ class Modal extends React.Component {
                     })
                   });
                 }
-                console.log("Current Index After Image Switch: " + this.state.currentIndex);
               }}>&gt;</h1>
             </div>
             <div className="project-info">
