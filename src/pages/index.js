c2e19eba9f043421d02a78a5b617bd7f9a13052f
Jeff Golota <60403549+JeffGolota7@users.noreply.github.com>
2022-07-20 14:12:33 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -63,7 +63,7 @@ const IndexPage = () => {
           <script async src="https://www.googletagmanager.com/gtag/js?id=G-BW0MYT4KL3"></script>
           <script>
             window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments)}
+            function gtag(){window.dataLayer.push(arguments)}
             gtag('js', new Date());
 
             gtag('config', 'G-BW0MYT4KL3');
